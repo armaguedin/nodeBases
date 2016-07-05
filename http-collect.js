8e8712604a053db943affb67bcaf3bc12eb49881
armaguedin <oussama.lakhdar@gmail.com>
2016-07-05 13:36:55 +0200

    Create http-collect.js

---
+++ http-collect.js
@@ -0,0 +1,33 @@
+     var http = require('http')  
+     var bl = require('bl')  
+       
+     http.get(process.argv[2], function (response) {  
+       response.pipe(bl(function (err, data) {  
+         if (err)  
+           return console.error(err)  
+         data = data.toString()  
+         console.log(data.length)  
+         console.log(data)  
+       }))    
+     })  
+     
+     
+     
+     /*
+     
+var http = require('http');
+var bl   = require('bl');
+
+
+http.get(process.argv[2], function (response){
+    response.pipe(bl(function (err, data){
+        if(err) return console.error(err);
+        console.log(data.toString().length);
+        console.log(data.toString());
+    }));
+    
+}).on('error', console.error);
+
+     
+     
+     */
